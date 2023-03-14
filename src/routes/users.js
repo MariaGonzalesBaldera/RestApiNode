73b7b293ca97f3779d8e625983d3a37246fe423f
maria <cristinabelen97@gmail.com>
2023-03-14 10:14:37 -0500

    show users

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,8 +3,7 @@ const routerUser = Router();
 import fetch from 'node-fetch';
 routerUser.get('/',async (req,res)=>{
         const response = await fetch('https://jsonplaceholder.typicode.com/users')
-        const users =await response.json()
-        console.log(users)
-        res.send('users')
+        const users = await response.json()
+        res.json(users)
     })
 export default routerUser
